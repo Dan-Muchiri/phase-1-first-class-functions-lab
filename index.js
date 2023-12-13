c0de0e928a215d43ddee4556c554fc34dfbd67ac
Dan Muchiri <dan.muchiri@student.moringaschool.com>
2023-12-13 13:23:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr){
+   return arr.slice(0,2);
+};
+const returnLastTwoDrivers = function(arr){
+    return arr.slice(-2);
+};
+let selectingDrivers = [];
+selectingDrivers.push(returnLastTwoDrivers);
+selectingDrivers.unshift(returnFirstTwoDrivers);
+function createFareMultiplier(integer){
+    return function(){
+        return (integer*5);
+    };
+}
+const doublerTripler = createFareMultiplier();
+function fareDoubler(doublerTripler){
+    return (2*doublerTripler);
+}
+function fareTripler(doublerTripler){
+    return (3*doublerTripler);
+}
+function selectDifferentDrivers(array,driverFunction){
+    return driverFunction(array);
+}
